bfea5b907045a8e5054953ad05dcbd69826d1299
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2023-08-31 21:31:30 -0700

    Sync prime-factors exercises with problem-specifications

--- exercises/practice/prime-factors/test_prime-factors.R
+++ exercises/practice/prime-factors/test_prime-factors.R
@@ -17,16 +17,46 @@ test_that("prime number", {
                c(2))
 })
 
+test_that("another prime number", {
+  number <- 3
+  expect_equal(prime_factors(number),
+               c(3))
+})
+
 test_that("square of a prime", {
   number <- 9
   expect_equal(prime_factors(number),
                c(3, 3))
 })
 
+test_that("product of first prime", {
+  number <- 4
+  expect_equal(prime_factors(number),
+               c(2, 2))
+})
+
 test_that("cube of a prime", {
   number <- 8
   expect_equal(prime_factors(number),
                c(2, 2, 2))
+})
+
+test_that("product of second prime", {
+  number <- 27
+  expect_equal(prime_factors(number),
+               c(3, 3, 3))
+})
+
+test_that("product of third prime", {
+  number <- 625
+  expect_equal(prime_factors(number),
+               c(5, 5, 5, 5))
+})
+
+test_that("product of first and second prime", {
+  number <- 6
+  expect_equal(prime_factors(number),
+               c(2, 3))
 })
 
 test_that("product of primes and non-primes", {
@@ -40,6 +70,7 @@ test_that("product of primes", {
   expect_equal(prime_factors(number),
                c(5, 17, 23, 461))
 })
+
 test_that("factors include a large prime", {
   number <- 93819012551
   expect_equal(prime_factors(number),
